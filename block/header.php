5f2cae675f51e6aecb8054ba7366269859277b6e
Amadou Barro <barroama23@gmail.com>
2024-03-05 13:12:48 +0000

    mise a niveau des formulaires et des pages

--- block/header.php
+++ block/header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/designsystem.css">
 </head>
 <body>
 <nav class="navbar">
@@ -25,7 +24,11 @@
     nav{
         display: grid;
         grid-template-columns: repeat(2,1fr);
-
+        background-color: wheat;
+    }
+    nav ul{
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
     }
 </style>
 </body>
